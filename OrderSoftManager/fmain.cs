8ea109adccd3c3b8d62bf4bd334239a694858b0a
duyndtpk00050 <duyndtpk00050@gmail.com>
2015-09-17 10:54:57 +0700

    Changed

--- OrderSoftManager/fmain.cs
+++ OrderSoftManager/fmain.cs
@@ -15,5 +15,65 @@ namespace OrderSoftManager
         {
             InitializeComponent();
         }
+
+        private void pbbanner_Click(object sender, EventArgs e)
+        {
+            fthongtin frm = new fthongtin();
+            frm.Show();
+        }
+
+        private void btndoimk_Click(object sender, EventArgs e)
+        {
+            fdoimk frm = new fdoimk();
+            frm.Show();
+        }
+
+        private void pbnhaphang_Click(object sender, EventArgs e)
+        {
+            fnhaphang frm = new fnhaphang();
+            frm.Show();
+        }
+
+        private void pbbanhang_Click(object sender, EventArgs e)
+        {
+            fbanhang frm = new fbanhang();
+            frm.Show();
+        }
+
+        private void pbthongke_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pbphanquyen_Click(object sender, EventArgs e)
+        {
+            fphanquyen frm = new fphanquyen();
+            frm.Show();
+        }
+
+        private void pbkhachhang_Click(object sender, EventArgs e)
+        {
+            fkh frm = new fkh();
+            frm.Show();
+        }
+
+        private void pbnv_Click(object sender, EventArgs e)
+        {
+            fnv frm = new fnv();
+            frm.Show();
+        }
+
+        private void pbmathang_Click(object sender, EventArgs e)
+        {
+            fmh frm = new fmh();
+            frm.Show();
+        }
+
+        private void pbncc_Click(object sender, EventArgs e)
+        {
+            fncc frm = new fncc();
+            frm.Show();
+        }
+
     }
 }
---
+++ OrderSoftManager/fthongtin.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace OrderSoftManager
+{
+    partial class fthongtin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fthongtin));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 112);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Công ty THHH Phong Vân\r\n186 Phan Bội Châu\r\nwww.phongvan.vn\r\n05003. 861 456 - 0984" +
+    " 956 567";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Silver;
+            this.textBox2.Location = new System.Drawing.Point(12, 130);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(411, 28);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Phần mềm quản lý bán hàng - Design by SVFPT";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // fthongtin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(435, 170);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fthongtin";
+            this.Text = "Thông tin ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
