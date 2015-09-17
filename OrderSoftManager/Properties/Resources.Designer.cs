fe511d005f20769dcaf80fc9dda914b7bd451399
duyndtpk00050 <duyndtpk00050@gmail.com>
2015-09-17 16:14:06 +0700

    Changed

--- OrderSoftManager/Properties/Resources.Designer.cs
+++ OrderSoftManager/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace OrderSoftManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap product {
+            get {
+                object obj = ResourceManager.GetObject("product", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap products {
             get {
                 object obj = ResourceManager.GetObject("products", resourceCulture);
