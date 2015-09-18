37f8a2bdd9b8f68c4bf0f060123c2dea5f9cf0ee
duyndtpk00050 <duyndtpk00050@gmail.com>
2015-09-19 00:22:20 +0700

    Changed

--- Database/connectDatabase.cs
+++ Database/connectDatabase.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Database
 {
-    class connectDatabase
+    public class connectDatabase
     {
+        private string str = "Data Source=VOSTRO2420;Initial Catalog=DATN;Integrated Security=True";
+        private SqlCommand cmd = null;
+        private SqlConnection conn = null;
+        private SqlDataReader dr = null;
+
+        public connectDatabase()
+        {
+            conn = new SqlConnection(str);
+        }
+        public void openConn()
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+        }
     }
 }
